e00b892e156dfcdded89818ac5f69107777122b5
Gavin Rigsby <grigsby@espsolution.net>
2023-02-07 07:19:05 -0800

    Update Front Page Info

--- .tina/config.ts
+++ .tina/config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "tinacms";
+import { clientId, token } from "./auth.js";
 
 // Your hosting provider likely exposes this as an environment variable
 const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
   branch,
-  clientId: "d031f7a2-c52e-4dd8-a916-d68e250e3fad", // Get this from tina.io
+  clientId, // Get this from tina.io
   token: "39230051eb85f886c315e5892632036c900f27a4", // Get this from tina.io
   build: {
     outputFolder: "admin",
@@ -39,6 +40,7 @@ export default defineConfig({
           },
         ],
       },
+      
     ],
   },
 });
